130da5f83fa4e76353a659af47537b0459e2e1b7
malik <malik.h@webdevpro.net>
2021-01-29 17:05:18 +0100

    fin du tp final

--- jour5-projet-final/js/diaporama.js
+++ jour5-projet-final/js/diaporama.js
@@ -1,8 +1,8 @@
 let diapositives = [
-    "https://via.placeholder.com/300x200?text=diapo2",
-    "https://via.placeholder.com/300x200?text=diapo3",
-    "https://via.placeholder.com/300x200?text=diapo4",
-    "https://via.placeholder.com/300x200?text=diapo5",
+    "https://via.placeholder.com/1200x300?text=diapo2",
+    "https://via.placeholder.com/1200x300?text=diapo3",
+    "https://via.placeholder.com/1200x300?text=diapo4",
+    "https://via.placeholder.com/1200x300?text=diapo5",
 ];
 
 let index = 0 ;
